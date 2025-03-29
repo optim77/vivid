36ca1fdf018c3bca0b25e7de0e8227078c98a38f
Kuba <plajerowy@gmail.com>
2025-03-29 10:32:12 +0100

    add sub domain fuzzer list

--- cli/cli.py
+++ cli/cli.py
@@ -5,7 +5,7 @@ from fuzzer.fuzzer import fuzzer
 from scanner.gravatar.gravatar import gravatar
 from scanner.username.scanner import scanner
 import pkgutil
-
+from sub_fuzzer.sub_fuzzer import run_fuzzer
 async def cli():
     console = Console()
     intro = """
@@ -23,6 +23,7 @@ async def cli():
     parser = argparse.ArgumentParser(description="Welcome to vivid")
     parser.add_argument("-u", type=str, nargs="?", help="Username to check")
     parser.add_argument("-e", type=str, nargs="?", help="Email to check")
+    parser.add_argument("-s", type=str, nargs="?", help="Domain to sub fuzzing")
     parser.add_argument("-p", type=str, action="store_true", default=False, help="Use inserted proxy")
     parser.add_argument("-d", type=str, nargs="+", help="Domain to validate a single domain (e.g., gmail) or a collection (e.g., gmail, amazon)")
     parser.add_argument("-l", action="store_true", help="List available domains")
@@ -43,7 +44,7 @@ async def cli():
     if args.gravatar:
         gravatar(args.gravatar)
 
-    if  args.e == '' or args.u == '' or args.f == '':
+    if  args.e == '' or args.u == '' or args.f == '' or args.s == '':
         console.print("Error: Need to specify action and pass arguments")
         parser.print_help()
         return
@@ -54,6 +55,9 @@ async def cli():
 
     if args.f:
         await fuzzer(args.f, proxy=args.p, csv_output=args.csv)
+
+    if args.s:
+        run_fuzzer(args.s, use_proxy=args.p, export_csv=args.csv)
 
     #console.print(f"Inserted mail: {args.email}")
 
